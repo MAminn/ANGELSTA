74303b0d3d594e6ef2e0b52d8d27f0dbcb2fa527
muhammed Borham <muhammeddamiinn@gmail.com>
2024-10-13 14:36:54 +0300

    mobile nav closing has been fixed

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,5 +1,10 @@
 "use client";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { links } from "@/constants";
 import Link from "next/link";
 import { useState } from "react";
@@ -11,11 +16,10 @@ interface NavMenuProps {
   session: Session | null;
 }
 
-
 const MobileNav: React.FC<NavMenuProps> = (props) => {
-   const session = props.session;
+  const session = props.session;
   const [submenuOpen, setSubmenuOpen] = useState(false);
-  if (session) { 
+  if (session) {
     return (
       <section className='md:hidden mt-2'>
         <Sheet>
@@ -28,7 +32,9 @@ const MobileNav: React.FC<NavMenuProps> = (props) => {
                 <li key={index} className='relative group py-6'>
                   <li key={index}>
                     {!link.sublinks ? (
-                      <Link href={link.path}>{link.name}</Link>
+                      <SheetClose asChild>
+                        <Link href={link.path}>{link.name}</Link>
+                      </SheetClose>
                     ) : (
                       <div>
                         <button
@@ -42,12 +48,11 @@ const MobileNav: React.FC<NavMenuProps> = (props) => {
                         {submenuOpen && (
                           <div className='space-y-2'>
                             {link.sublinks.map((sub, idx) => (
-                              <Link
-                                key={idx}
-                                href={sub.path}
-                                className='block py-2'>
-                                {sub.name}
-                              </Link>
+                              <SheetClose key={idx} asChild>
+                                <Link href={sub.path} className='block py-2'>
+                                  {sub.name}
+                                </Link>
+                              </SheetClose>
                             ))}
                           </div>
                         )}
@@ -64,48 +69,50 @@ const MobileNav: React.FC<NavMenuProps> = (props) => {
         </Sheet>
       </section>
     );
-    } else {return (
-    <section className='md:hidden mt-2'>
-      <Sheet>
-        <SheetTrigger>
-          <FaBars size={24} />
-        </SheetTrigger>
-        <SheetContent className='bg-[#001d21f3] text-white flex justify-center w-full'>
-          <ul className="mt-8 text-center">
-            {links.map((link, idx) => (
-              <li key={idx} className="py-6">
-                {!link.sublinks ? (
-                  <Link href={link.path}>{link.name}</Link>
-                ) : (
-                  <div>
-                    <button
-                      onClick={() => setSubmenuOpen(!submenuOpen)}
-                      className='flex items-center justify-between w-full py-2'>
-                      <span>{link.name}</span>
-                      <FaChevronDown />
-                    </button>
-                    {submenuOpen && (
-                      <div className='ml-4 space-y-2'>
-                        {link.sublinks.map((sub, idx) => (
-                          <Link
-                            key={idx}
-                            href={sub.path}
-                            className='block py-2'>
-                            {sub.name}
-                          </Link>
-                        ))}
-                      </div>
-                    )}
-                  </div>
-                )}
-              </li>
-            ))}
-          </ul>
-        </SheetContent>
-      </Sheet>
-    </section>
-  );} 
-  
+  } else {
+    return (
+      <section className='md:hidden mt-2'>
+        <Sheet>
+          <SheetTrigger>
+            <FaBars size={24} />
+          </SheetTrigger>
+          <SheetContent className='bg-[#001d21f3] text-white flex justify-center w-full'>
+            <ul className='mt-8 text-center'>
+              {links.map((link, idx) => (
+                <li key={idx} className='py-6'>
+                  {!link.sublinks ? (
+                    <SheetClose asChild>
+                      <Link href={link.path}>{link.name}</Link>
+                    </SheetClose>
+                  ) : (
+                    <div>
+                      <button
+                        onClick={() => setSubmenuOpen(!submenuOpen)}
+                        className='flex items-center justify-between w-full py-2'>
+                        <span>{link.name}</span>
+                        <FaChevronDown />
+                      </button>
+                      {submenuOpen && (
+                        <div className='ml-4 space-y-2'>
+                          {link.sublinks.map((sub, idx) => (
+                            <SheetClose key={idx} asChild>
+                              <Link href={sub.path} className='block py-2'>
+                                {sub.name}
+                              </Link>
+                            </SheetClose>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </SheetContent>
+        </Sheet>
+      </section>
+    );
+  }
 };
 
 export default MobileNav;
